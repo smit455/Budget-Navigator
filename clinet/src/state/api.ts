9822bffe31d64d340e8c7537d6046d042b2f4a99
smit <ajanismit5@gmail.com>
2025-01-24 20:13:04 +0530

    fix backend-url

--- clinet/src/state/api.ts
+++ clinet/src/state/api.ts
@@ -2,7 +2,7 @@ import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { GetKpisResponse,GetProductsResponse,GetTransactionsResponse } from "./types";
 
 export const api = createApi({
-    baseQuery:fetchBaseQuery({baseUrl:'http://localhost:1337'}),
+    baseQuery:fetchBaseQuery({baseUrl:'https://budget-navigator-5.onrender.com'}),
     reducerPath:"main",
     tagTypes:["Kpis","Products","Transactions"],
     endpoints:(build)=>({
